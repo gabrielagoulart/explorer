80f6e11259fb30f5d780f97545d32be3329a9a8c
Gabriela Goulart <101572408+gabrielagoulart@users.noreply.github.com>
2022-06-10 18:51:56 -0300

    js

---
+++ main.js
@@ -0,0 +1,45 @@
+const elementoResposta = document.querySelector("#resposta")
+const inputPergunta = document.querySelector("#inputPergunta")
+const respostas = [
+    "Certeza!",
+    "Não tenho tanta certeza.",
+    "É decididamente assim.",
+    "Não conte com isso.",
+    "Sem dúvidas!",
+    "Pergunte novamente mais tarde.",
+    "Sim, definitivamente!",
+    "Minha resposta é não.",
+    "Você pode contar com isso.",
+    "Melhor não te dizer agora.",
+    "A meu ver, sim.",
+    "Minhas fontes dizem não.",
+    "Provavelmente.",
+    "Não é possível prever agora.",
+    "As perspectivas não são tão boas.",
+    "Sim.",
+    "Concentre-se e pergunte  novamente.",
+    "Sinais apontam que sim."
+]
+
+//CLICAR EM FAZER PERGUNTA
+function fazerPergunta() {
+
+    if(inputPergunta.value == "") {
+        alert('Digite sua pergunta')
+        return
+    } 
+
+    const pergunta = "<div>" + inputPergunta.value + "</div>"
+   
+    //GERAL NÚMERO ALEATÓRIO
+    const totalRespostas = respostas.length
+    const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+
+    elementoResposta.innerHTML = pergunta + respostas[numeroAleatorio]
+
+    /*//SUMIR A RESPOSTA DEPOIS DE 3 SEGUNDOS
+    setTimeout(function() {
+        elementoResposta.style.opacity = 0;
+    }, 3000) */
+
+}
